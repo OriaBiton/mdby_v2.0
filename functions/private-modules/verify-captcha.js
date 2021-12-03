71675a285672c11ae78c57ffdca9e1b0393343f7
OriaBiton <oriabiton@gmail.com>
2021-12-03 13:45:10 +0200

    security fix

--- functions/private-modules/verify-captcha.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-module.exports = async function(token){
-  const secret = '6LfZo4caAAAAAJV9ZXRfEvlJ5TWA9a0vK8IEwC9z';
-  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
-  const response = (await (await fetch(url, {method: 'POST'})).json());
-  if (!response.success) throw new Error('Bad captcha');
-}
